9e219c67f5c2580d48f05e67def0032ed98c1f5a
mathmods <mathias.beverfjord@gmail.com>
2014-10-06 11:03:17 +0200

    Random

--- src/net/mathmods/projectgame/Screen.java
+++ src/net/mathmods/projectgame/Screen.java
@@ -1,5 +1,7 @@
 package net.mathmods.projectgame;
 
+import java.awt.Graphics;
+
 import javax.swing.JPanel;
 
 public class Screen extends JPanel implements Runnable {
@@ -8,18 +10,31 @@ public class Screen extends JPanel implements Runnable {
 	Frame frame;
 	public boolean running = false;
 	private int fps = 0;
+	public static int scene = 0;
 	
 	public Screen(Frame frame){
 		this.frame = frame;
 	}
 	
 	@Override
+	public void paintComponents(Graphics g){
+		
+		
+		
+	}
+	
+	@Override
 	public void run() {
+		
 		float lastTime = System.currentTimeMillis();
+		
 		running = true;
 		int frames = 0;
+		
 		while(running){
+			
 			repaint();
+			
 			frames++;
 			if(lastTime <= System.currentTimeMillis()){
 				fps = frames;
